b39e6bd450689adf9d2739cb17301dacb6b52766
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-05-28 16:33:19 +0200

    show.blade con link in index. Fix template

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,33 +3,36 @@
 @section('title', 'Dc Comics')
 
 @section('content')
-<main>
-    <div class="container">
-        <h1>
-            Current Series
-        </h1>
-        <div class="row">
 
-            @foreach ($comics as $comic)
-                <div class="col-12 col-sm-6 col-md-4 mb-4">
-                    <div class="card">
-                        <img src="{{ $comic->image }}" class="card-img-top" alt="{{ $comic->title }}" />
-                        <span class="text-white position-absolute spanpop shadow-lg"
-                            style="display: none;">{{ $comic['description'] }}</span>
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $comic->series }}</h5>
-                            <p>
-                                Price: <span>{{ $comic->price }}</span>
-                            </p>
-                            <p>
-                                Type: <span>{{ $comic->type }}</span>
-                            </p>
-                        </div>
+<div class="container">
+    <h1>
+        Current Series
+    </h1>
+    <div class="row">
+
+        @foreach ($comics as $comic)
+            <div class="col-12 col-sm-6 col-md-4 mb-4">
+                <div class="card">
+                    <img src="{{ $comic->image }}" class="card-img-top" alt="{{ $comic->title }}" />
+                    <span class="text-white position-absolute spanpop shadow-lg"
+                        style="display: none;">{{ $comic['description'] }}</span>
+                    <div class="card-body">
+                        <h4 class="card-title">{{ $comic->title }}</h4>
+                        <h5 class="card-title">{{ $comic->series }}</h5>
+                        <p>
+                            Price: <span>{{ $comic->price }}</span>
+                        </p>
+                        <p>
+                            Type: <span>{{ $comic->type }}</span>
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="{{route('comics.show', $comic->id)}}" class="btn btn-primary">Vedi dettaglio</a>
                     </div>
                 </div>
-            @endforeach
+            </div>
+        @endforeach
     </div>
-    </div>
-</main>
+</div>
 
 @endsection
