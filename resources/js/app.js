88ef08277a4113d84695f801ec7c721b27df720c
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-05-29 16:11:42 +0200

    add modal screen to delete from show

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,19 @@ import * as bootstrap from 'bootstrap';
 // import.meta.glob([
 //     '../img/**'
 // ])
+
+/* modale to delete */
+const deleteButton = document.getElementById("comicDelete");
+//console.log(deleteButton);
+deleteButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const modale = document.getElementById("exampleModal");
+    const myModal = new bootstrap.Modal(modale);
+    myModal.show();
+    const btnSave = modale.querySelector('.btn-danger');
+    // console.log(btnSave);
+    btnSave.addEventListener('click', ()=>{
+        //invia il form che è il suo parent 
+        deleteButton.parentElement.submit(); 
+    })
+});
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -28,6 +28,24 @@
             </form>
         </div>
     </div>
-
 </section>
+<!-- Modal -->
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5 color-danger" id="exampleModalLabel">Conferma eliminazione</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                Sei sicuro che vuoi elimnare l'elemento?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Torna indietro</button>
+                <button type="button" class="btn btn-danger">Sì elimina!</button>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
